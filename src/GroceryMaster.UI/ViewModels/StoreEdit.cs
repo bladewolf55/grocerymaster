addba6eb5a4d50db45527fe097f7004417ddc1a3
Charles L Flatt <charles@tfhome.us>
2022-10-16 12:48:34 -0600

    Add and delete stores

--- src/GroceryMaster.UI/ViewModels/StoreEdit.cs
+++ src/GroceryMaster.UI/ViewModels/StoreEdit.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GroceryMaster.Maui.Maui.Pages;
 using System.Collections.ObjectModel;
 
 namespace GroceryMaster.Maui.Maui.ViewModels;
@@ -6,30 +8,65 @@ namespace GroceryMaster.Maui.Maui.ViewModels;
 public partial class StoreEdit : ObservableObject
 {
     private readonly IGroceryDataService service;
-    private Store selectedStore;
+
+    [ObservableProperty]
+    ObservableCollection<Store> stores;
+
+    [ObservableProperty]
+    Store selectedStore;
+
+    [ObservableProperty]
+    string selectedStoreName;
+
+    [ObservableProperty]
+    string newStoreName;
 
     public StoreEdit(IGroceryDataService service)
     {
         this.service = service;
-        Stores = new ObservableCollection<Store>(GetStores());
+        Refresh();
     }
 
-    public Store SelectedStore
+    partial void OnSelectedStoreChanged(Store value)
     {
-        get
-        {
-            Application.Current.MainPage.DisplayAlert("Selected", $"Get {selectedStore.Name}", "OK");
-            return selectedStore;
-        }
-        set
-        {
-            var name = value != null ? value.Name : "na";
-            Application.Current.MainPage.DisplayAlert("Selected", $@"Set {name}", "OK");
-            SetProperty(ref selectedStore, value);
-        }
+        SelectedStoreName = value.Name;
     }
 
-    public ObservableCollection<Store> Stores { get; private set; }
+    [RelayCommand]
+    private void Refresh()
+    {
+        Stores = new ObservableCollection<Store>(GetStores());        
+    }
+
+    [RelayCommand]
+    private void AddStore()
+    {
+        if (String.IsNullOrWhiteSpace(NewStoreName))
+        {
+            Application.Current.MainPage.DisplayAlert("Error","Name is required", "OK");
+            return;
+        }
+        try
+        {
+            Store store = new() { Name = NewStoreName };
+            store = service.AddStore(store);
+            service.SaveChanges();
+            Stores.Add(store);
+            NewStoreName = "";
+        }
+        catch (Exception ex)
+        {
+            Application.Current.MainPage.DisplayAlert("Error", ex.GetBaseException().Message, "OK");
+        }
+    }
+    [RelayCommand]
+    private void DeleteStore(object store)
+    {
+        var x = (Store)store;
+        service.DeleteStore(x);
+        service.SaveChanges();
+        Stores.Remove(x);
+    }
 
     public IEnumerable<Store> GetStores() => service.GetStores();
 }
